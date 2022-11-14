2d433ee6a1d611ac2c46258255a04b1ff3baad29
Slava Petrov <92324273+slavapmk@users.noreply.github.com>
2022-11-14 15:16:19 +0300

    Add files via upload

---
+++ str_easy.h
@@ -0,0 +1,41 @@
+//
+// Created by Sch1561-it-30 on 14.11.2022.
+//
+
+#ifndef UNTITLED_STR_EASY_H
+#define UNTITLED_STR_EASY_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string itc_hello_str(const string &name);
+
+long long itc_len(const string &str);
+
+void itc_print_copy_str(const string &str, int number);
+
+void itc_first_end_three(string str);
+
+int itc_count_char_in_str(char ch, const string &str);
+
+string itc_even_place(const string &str);
+
+double itc_percent_lower_uppercase(const string &str);
+
+string itc_reverse_str(const string &str);
+
+string itc_slice_str(const string &str, int start, int end);
+
+bool itc_equal_reverse(const string &str);
+
+string itc_cmp_str(const string &str1, const string &str2, int num);
+
+int itc_find_str(string str1, string str2);
+
+string itc_three_str(string str1, string str2, string str3);
+
+int itc_max_char_on_end(const string &str);
+
+#endif //UNTITLED_STR_EASY_H
