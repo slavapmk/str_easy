46ff408cf0368fa73afa7772add67b435b4ec1c0
Slava Petrov <slavapmk@gmail.com>
2022-11-28 22:30:00 +0300

    microfix

--- str_process.cpp
+++ str_process.cpp
@@ -43,7 +43,9 @@ string itc_slice_str(const string &str, int start, int end) {
 string itc_cmp_str(string str1, string str2, int num) {
     string first = itc_slice_str(str1, 0, num - 1);
     string third = itc_slice_str(str1, num, (int) (itc_len(str1) - itc_len(str2)) - 1);
-    return first + str2 + third;
+    string result = first + str2 + third;
+    result = itc_slice_str(result, 0, (int) itc_len(str1) - 1);
+    return result;
 }
 
 string itc_three_str(string str1, string str2, string str3) {
